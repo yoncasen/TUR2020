b9d95a3d9bfeb6b6ebd684fbbf570122594a6a5d
Yonca <73234933+yoncasen@users.noreply.github.com>
2024-04-20 19:00:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello,World!")
+import discord
+from discord.ext import commands
+from bot_token import token
+from password_generator import gen_pass
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def parola(ctx):
+    password = gen_pass(10)
+    await ctx.send(password)
+
+bot.run(token)
